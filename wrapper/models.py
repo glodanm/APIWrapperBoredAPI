d6fd16d6f0fb699aafd388116fbc0163dab939f1
glodanm <glodan196915@gmail.com>
2023-09-01 15:09:50 +0300

    Create model

--- wrapper/models.py
+++ wrapper/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Activity(models.Model):
+    activity = models.CharField(max_length=200)
+    type = models.CharField(max_length=50)
+    participants = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    accessibility = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.activity
+    
