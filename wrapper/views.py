582eee108a84e5817f61cae03d6793b3eb1d0b81
glodanm <glodan196915@gmail.com>
2023-09-03 12:01:08 +0300

    Add filters for data

--- wrapper/views.py
+++ wrapper/views.py
@@ -10,18 +10,34 @@ class GetActivityAPI(APIView):
 
     def get(self, request):
         data = request.data
+        type = request.GET.get('type')
+        participants = request.GET.get('participants')
+        minprice = request.GET.get('minprice')
+        maxprice = request.GET.get('maxprice')
+        minaccessibility = request.GET.get('minaccessibility')
+        maxaccessibility = request.GET.get('maxaccessibility')
+
         wrapper = Wrapper()
+
         activity_data = wrapper.get_activity(
-            activity_type=data.get('type'),
-            participants=data.get('participants'),
-            minprice=data.get('minprice'),
-            maxprice=data.get('maxprice'),
-            minaccessibility=data.get('minaccessibility'),
-            maxaccessibility=data.get('maxaccessibility'),
+            activity_type=type,
+            participants=participants,
+            minprice=minprice,
+            maxprice=maxprice,
+            minaccessibility=minaccessibility,
+            maxaccessibility=maxaccessibility,
         )
-        serializer = ActivitySerializer(data=activity_data)
-        if serializer.is_valid():
-            serializer.save()
+
+        activity = Activity(
+            activity=activity_data['activity'],
+            type=activity_data['type'],
+            participants=activity_data['participants'],
+            price=activity_data['price'],
+            accessibility=activity_data['accessibility'],
+            link=activity_data.get('link', 'No URL'),
+        )
+        activity.save()
+        serializer = ActivitySerializer(activity)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
         
@@ -29,6 +45,32 @@ class ActivityList(APIView):
 
     def get(self, request):
         activity = Activity.objects.all()
+        
+        activity_type = self.request.query_params.get('type', None)
+        participants = self.request.query_params.get('participants', None)
+        minprice = self.request.query_params.get('minprice', None)
+        maxprice = self.request.query_params.get('maxprice', None)
+        minaccessibility = self.request.query_params.get('minaccessibility', None)
+        maxaccessibility = self.request.query_params.get('maxaccessibility', None)
+
+        if activity_type:
+            activity = activity.filter(type__iexact=activity_type)
+
+        if participants is not None:
+            activity = activity.filter(participants__iexact=participants)
+
+        if minprice is not None:
+            activity = activity.filter(price__gte=minprice)
+
+        if maxprice is not None:
+            activity = activity.filter(price__lte=maxprice)
+
+        if minaccessibility is not None:
+            activity = activity.filter(accessibility__gte=minaccessibility)
+
+        if maxaccessibility is not None:
+            activity = activity.filter(accessibility__lte=maxaccessibility)
+
         serializer = ActivitySerializer(activity, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
